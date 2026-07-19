5114c878760c8136a8bf3766a2dbcea1b8ea3925
rbautoscrap <s94002207@gmail.com>
2026-07-19 15:06:14 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,16 @@ export default function RootLayout({
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Syne:wght@600;700;800&display=swap"
+        />
       </head>
       <body>
         <PartnerBanner />
